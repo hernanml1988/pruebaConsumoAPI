67edab9afd2a55f687a816e483621e7aa5fe4398
hernanml1988 <97610098+hernanml1988@users.noreply.github.com>
2023-03-05 19:05:57 -0400

    tabla de todos los personajes creada, paginacion de la tabla creada y se crea el formulario de busqueda po especie y estado(solo esta creado aun no funciona)

---
+++ app/Http/Controllers/InicioController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class InicioController extends Controller
+{
+    public function index(){
+
+        $url = env('URL_API', 'https://rickandmortyapi.com/api/character');
+        $res = Http::get($url);
+        $arrayRes = $res->json();
+        $personajes = $arrayRes['results'];
+        
+
+        $especies = array();
+        $estados = array();
+
+        //realizamo un ciclo para poder obtener todas las especies  y los estados
+        for ($i=1; $i < $arrayRes['info']['pages']; $i++) { 
+             $res_aux = Http::get($url.'/?page='.$i);
+             $arrayRes_aux = $res_aux->json();
+             $personajes_aux = $arrayRes_aux['results'];
+
+             foreach ($personajes_aux as $key ) {
+                if(!in_array($key['species'], $especies)){
+                    $especies[] = $key['species'];
+                     
+                }
+             }
+             
+
+             foreach ($personajes_aux as $key ) {
+                 if(!in_array($key['status'], $estados)){                     
+                     $estados[] = $key['status'];
+                }
+                
+             }
+            
+        }    
+        
+        return view('/welcome', ['personajes' => $personajes, 'especies' => $especies, 'estados' => $estados, 'pagina' => 1]);
+    }
+
+
+    
+ 
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [InicioController::class, 'index']);
+Route::get('/mostrar_detale/{id}', [InicioController::class, 'mostrarDetalle'])->name('mostrar_detalle');
+
+
+
+?>
